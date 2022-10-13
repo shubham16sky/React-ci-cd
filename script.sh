360945a40216500e126d9e90810852c33066656a
shubham16sky <shubhampc16@gmail.com>
2022-10-14 01:31:12 +0530

    modified script

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ container=`sudo docker ps | awk '{print $NF}' | awk '(NR>1)' | grep -E "testing"
 
 if [ ! -z "$container" ]
 then
-        sudo docker stop project && sudo docker rm project  && sudo docker run --name "project" --detach  frontend
+        sudo docker stop project && sudo docker rm project  && sudo docker run --name "project" --detach 3000:3000  frontend
 
 else
-        sudo docker run --name "project"  -p 3000:3000  frontend
+        sudo docker run --name "project" --detach  -p 3000:3000  frontend
 fi
